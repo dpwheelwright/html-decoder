68ae2c8199e20fb30a7f29db0e66f475099998e5
dpwheelwright <dpwheelwright@att.net>
2014-12-28 18:58:44 -0600

    summary

---
+++ main.mm
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  html-decoder
+//
+//  Created by Demitrius Wheelwright on 12/28/14.
+//  Copyright (c) 2014 Demitrius Wheelwright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <iostream>
+
+int main(int argc, char * argv[]) {
+    
+    std::cout << "Goodbye, universe!" << std::endl;
+    
+    const char * a = "let it rock - kevin rudolph";
+    
+    return 10; 
+}
